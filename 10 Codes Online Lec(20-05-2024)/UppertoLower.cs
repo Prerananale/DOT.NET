51fb6eb3568f34399d3c0646b53567a7c063077e
Prerana Nale <121221427+Prerananale@users.noreply.github.com>
2024-06-24 12:10:59 +0530

    Add files via upload

---
+++ 10 Codes Online Lec(20-05-2024)/UppertoLower.cs
@@ -0,0 +1,11 @@
+
+using System;
+public class hello{
+    public static void Main(string[] args){
+        string name ="PRERANA";
+        
+        Console.WriteLine("UPPER CASE STRING IS : "+ name);
+        Console.WriteLine("LOWER CASE STRING IS : "+ name.ToLower());
+
+    }
+}
